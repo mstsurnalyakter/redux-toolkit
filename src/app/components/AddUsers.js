62b0de84fafda9db39689ecf807d7f49d620a7f0
mstsurnalyakter <surnalyms@gmail.com>
2024-10-12 16:01:30 +0600

    store data in redux

--- src/app/components/AddUsers.js
+++ src/app/components/AddUsers.js
@@ -1,11 +1,21 @@
-import React from 'react'
+"use client"
+import { useState } from 'react'
+import { addUser } from '../redux/slice';
+import { useDispatch } from 'react-redux';
 
 const AddUsers = () => {
+  const [name,setName]=useState("");
+  const dispatch = useDispatch();
+
+  const userDispatch = () =>{
+    dispatch(addUser(name))
+  }
+
   return (
     <div className='border-2 p-10'>
       <h3 className='text-4xl font-bold mb-1'>User List</h3>
-      <input className='border-2 w-1/2 border-gray-600 p-5' type='text' placeholder='Add New User' />
-      <button className='bg-sky-600 px-5 py-5 text-gray-200'>Add User</button>
+      <input onChange={(e)=>setName(e.target.value)} className='border-2 w-1/2 border-gray-600 p-5' type='text' placeholder='Add New User' />
+      <button onClick={userDispatch} className='bg-sky-600 px-5 py-5 text-gray-200'>Add User</button>
 
     </div>
   )
