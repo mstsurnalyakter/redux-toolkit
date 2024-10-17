bd8911b985c7469be562d400b46758a062d15aea
mst-surnaly-akter <surnalyms@gmail.com>
2024-10-17 10:45:41 +0600

    add: localStore

--- src/app/redux/slice.js
+++ src/app/redux/slice.js
@@ -1,7 +1,40 @@
-const { createSlice,nanoid } = require("@reduxjs/toolkit");
+// const { createSlice,nanoid,current } = require("@reduxjs/toolkit");
+
+// const initialState = {
+//     users:[]
+// }
+
+// const Slice = createSlice({
+//     name:"addUserSlice",
+//     initialState,
+//     reducers:{
+//         addUser:(state,action)=>{
+//             console.log(action);
+//             const data={
+//                 id:nanoid(),
+//                 name:action.payload
+//             };
+            
+//             state.users.push(data);
+//             let userData = JSON.stringify(current(state.users));
+//             localStorage.setItem("users",userData);
+//             console.log(current(state.users));
+//         },
+//         removeUser:(state,action)=>{
+//             console.log(action);
+//             const data = state.users.filter(user=>user.id !== action.payload);
+//             state.users = data;
+//         }
+//     }
+// });
+
+// export const {addUser,removeUser} = Slice.actions;
+// export default Slice.reducer;
+
+const { createSlice,nanoid,current } = require("@reduxjs/toolkit");
 
 const initialState = {
-    users:[]
+    users:JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : []
 }
 
 const Slice = createSlice({
@@ -14,11 +47,18 @@ const Slice = createSlice({
                 id:nanoid(),
                 name:action.payload
             };
+            
             state.users.push(data);
+            let userData = JSON.stringify(current(state.users));
+            localStorage.setItem("users",userData);
+            console.log(current(state.users));
         },
         removeUser:(state,action)=>{
             console.log(action);
             const data = state.users.filter(user=>user.id !== action.payload);
+            let userInfo = JSON.stringify(data);
+            localStorage.setItem("users",userInfo)
+            // localStorage.removeItem("users",action.payload)
             state.users = data;
         }
     }
@@ -26,3 +66,4 @@ const Slice = createSlice({
 
 export const {addUser,removeUser} = Slice.actions;
 export default Slice.reducer;
+
